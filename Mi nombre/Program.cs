3e1764e4a69454b667add6ad604ff37464335317
Pablo Junior Perez <pablojrperezsantos@gmail.com>
2020-09-02 16:11:19 -0400

    Se pide el nombre del usuario y se le saluda

---
+++ Mi nombre/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mi_nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Como te llamas?");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Hola mi nombre es " + nom);
+            Console.ReadKey();
+        }
+    }
+}
